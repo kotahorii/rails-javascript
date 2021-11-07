4083392bc08a7f57ac2bb32b7b9d6d92da376274
kotahorii <khorii.micron@gmail.com>
2021-11-07 07:41:50 +0000

    finish lesson13

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,33 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
+
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+// let title = "javascript"
+// alert(title)
+
+// $(function(){
+//   $('.jquery').on('click', function () {
+//     $(this).css('color', 'red')
+//   })
+// })
+
+$(document).ready(function(){
+  $('#theTarget').skippr({
+    transition: 'slide',
+    speed: 1000,
+    easing: 'easeOutQuart',
+    navType: 'block',
+    childrenElementType: 'div',
+    arrows: true,
+    autoPlay: true,
+    autoPlayDuration: 3000,
+    keyboardOnAlways: true,
+    hidePrevious: false
+  })
+})
